18abf6740a8289bcca024b6946dac43ccb06b86e
Turcan Madalina <turcan.madalina@yahoo.com>
2020-11-13 19:58:16 -0500

    I have done the movement of the dog

--- Project/Toby.java
+++ Project/Toby.java
@@ -12,57 +12,94 @@ public class Toby extends Actor
     private GreenfootImage image2 = null;
     private GreenfootImage image3 = null;
     private GreenfootImage image4 = null;
-    
+    private GreenfootImage image5 = null;
+    private GreenfootImage image6 = null;
+    private GreenfootImage image7 = null;
+    private GreenfootImage image8 = null;
+    private GreenfootImage image9 = null;
+    private GreenfootImage image10 = null;
+    private GreenfootImage image11 = null;
+
 
 /**
  * 
  */  
 public Toby()
 {
-image1 =  new  GreenfootImage(".png");
-image2 =  new  GreenfootImage(".png");
-image3 =  new  GreenfootImage(".png");
-image4 =  new  GreenfootImage(".png");
+image1 =  new  GreenfootImage("dogside.png");
+image2 =  new  GreenfootImage("dogside2.png");
+image3 =  new  GreenfootImage("dogside3.png");
+image4 =  new  GreenfootImage("dogsideleft.png");
+image5 =  new  GreenfootImage("dogsideleft2.png");
+image6 =  new  GreenfootImage("dogback.png");
+image7 =  new  GreenfootImage("dogback2.png");
+image8 =  new  GreenfootImage("dogback3.png");
+image9 =  new  GreenfootImage("dogfront.png");
+image10 =  new  GreenfootImage("dogfront2.png");
+image11 =  new  GreenfootImage("dogfront3.png");
+
+
 }
 /**
-     * this method is controlling the direction of Toby
+ * Act - do whatever the Toby wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void keyDown()
-    {
-        if (Greenfoot.isKeyDown("up")) {
+public void act() 
+{
+ // Add your action code here.
+} 
+/**
+ * this method is controlling the direction of Toby
+ */
+ public void keyDown()
+ {
+     if (Greenfoot.isKeyDown("up")) {
             setLocation(getX(), getY() - 5);
+             if (getImage() == image6) {
+            setImage(image7);
+             }
+        else if (getImage() == image7) {
+            setImage(image8);
         }
-        if (Greenfoot.isKeyDown("down")) {
+        else if (getImage() == image8) {
+            setImage(image6);
+        }
+        }
+     if (Greenfoot.isKeyDown("down")) {
             setLocation(getX(), getY() + 5);
+             if (getImage() == image9) {
+            setImage(image10);
+             }
+        else if (getImage() == image10) {
+            setImage(image11);
         }
-        if (Greenfoot.isKeyDown("right")) {
+        else if (getImage() == image11) {
+            setImage(image9);
+        }
+        }
+     if (Greenfoot.isKeyDown("right")) {
             setLocation(getX() + 4, getY());
              if (getImage() == image1) {
-            setImage(image2);
-        }
+                setImage(image2);
+             }
         else if (getImage() == image2) {
             setImage(image3);
         }
         else if (getImage() == image3) {
             setImage(image1);
         }
+    }
         
-        if (Greenfoot.isKeyDown("left")) {
-            setLocation(getX() - 4, getY());
-            if (getImage() == image3) {
-                setImage(image4);
+    if (Greenfoot.isKeyDown("left")) {
+        setLocation(getX() - 4, getY());
+        if (getImage() == image4) {
+                setImage(image5);
             }
             else {
                 setImage(image4);
             }
         }
     }
-/**
-     * Act - do whatever the Toby wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act() 
-    {
-        // Add your action code here.
-    }    
+
+   
 }
